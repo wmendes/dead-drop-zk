cc98ae81e45229a68684150e3131ad93bdd52044
Wlad Mendes <wlademyr.mendes@gmail.com>
2026-02-23 15:03:59 -0300

    chore: Remove obsolete proof systems (risc0, typezero)

--- risc0/dead_drop_proof/host/src/bin/image-id.rs
+++
@@ -1,8 +0,0 @@
-use dead_drop_proof_host::digest_to_bytes;
-use dead_drop_proof_methods::DEAD_DROP_PROOF_GUEST_ID;
-use hex::encode as hex_encode;
-
-fn main() {
-    let bytes = digest_to_bytes(DEAD_DROP_PROOF_GUEST_ID.into());
-    println!("image_id: {}", hex_encode(bytes));
-}
